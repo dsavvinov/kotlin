e99815680bfc722d13a2c9e632f184c19c131012
dsavvinov <dsavvinov@gmail.com>
2017-09-06 11:24:58 +0300

    Fix wrong reference to receiver in contracts for isNullOrEmpty and isNullOrBlank

--- libraries/stdlib/src/kotlin/text/Strings.kt
+++ libraries/stdlib/src/kotlin/text/Strings.kt
@@ -228,7 +228,7 @@ public fun String.padEnd(length: Int, padChar: Char = ' '): String
 @kotlin.internal.InlineOnly
 public inline fun CharSequence?.isNullOrEmpty(): Boolean {
     contract {
-        returns(false) implies (this != null)
+        returns(false) implies (this@isNullOrEmpty != null)
     }
 
     return this == null || this.length == 0
@@ -262,7 +262,7 @@ public inline fun CharSequence.isNotBlank(): Boolean = !isBlank()
 @kotlin.internal.InlineOnly
 public inline fun CharSequence?.isNullOrBlank(): Boolean {
     contract {
-        returns(false) implies (this != null)
+        returns(false) implies (this@isNullOrBlank != null)
     }
 
     return this == null || this.isBlank()
